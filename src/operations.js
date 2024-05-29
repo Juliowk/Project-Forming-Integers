bc45d9c225b5db5c31a45be1b5f6f3f6af136022
Júlio Elias de Sousa rocha <julioeliasdesousarocha.work@gmail.com>
2024-05-29 15:03:49 -0300

    Pagination

--- src/operations.js
+++ src/operations.js
@@ -1,13 +1,13 @@
 const comparison = (fraction, integers, necessaryTimes) => {
-     const [numerator, denominator] = fraction.split('/').map(Number);
-     const total = (numerator / denominator) * necessaryTimes;
-     return total === integers;
+    const [numerator, denominator] = fraction.split('/').map(Number);
+    const total = (numerator / denominator) * necessaryTimes;
+    return total === integers;
 }
 
 const updateanswer = (id, fraction, integers, necessaryTimesId) => {
-     const necessaryTimes = document.getElementById(necessaryTimesId).value
-     document.getElementById(id).textContent = comparison(fraction, integers, necessaryTimes) ? "Correto" : "Errado";
-}
+    const necessaryTimes = document.getElementById(necessaryTimesId).value
+    document.getElementById(id).textContent = comparison(fraction, integers, necessaryTimes) ? "Correto" : "Errado";
+}   
 
 document.getElementById("necessaryTimes01").addEventListener("input", () => updateanswer("response01", "1/2", 1, "necessaryTimes01"));
 document.getElementById("necessaryTimes02").addEventListener("input", () => updateanswer("response02", "1/3", 1, "necessaryTimes02"));
