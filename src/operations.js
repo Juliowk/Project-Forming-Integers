c9c096952f08d29f81f5e990101a149f653f5778
Júlio Elias de Sousa rocha <julioeliasdesousarocha.work@gmail.com>
2024-05-29 16:45:14 -0300

    preparation to accept the pr

--- src/operations.js
+++ src/operations.js
@@ -5,12 +5,19 @@ const comparison = (fraction, integers, necessaryTimes) => {
 }
 
 const updateanswer = (id, fraction, integers, necessaryTimesId) => {
-    const necessaryTimes = document.getElementById(necessaryTimesId).value
-    document.getElementById(id).textContent = comparison(fraction, integers, necessaryTimes) ? "Correto" : "Errado";
-}   
+    const necessaryTimes = document.getElementById(necessaryTimesId).value;
 
-document.getElementById("necessaryTimes01").addEventListener("input", () => updateanswer("response01", "1/2", 1, "necessaryTimes01"));
-document.getElementById("necessaryTimes02").addEventListener("input", () => updateanswer("response02", "1/3", 1, "necessaryTimes02"));
-document.getElementById("necessaryTimes03").addEventListener("input", () => updateanswer("response03", "1/4", 1, "necessaryTimes03"));
-document.getElementById("necessaryTimes04").addEventListener("input", () => updateanswer("response04", "1/5", 1, "necessaryTimes04"));
-document.getElementById("necessaryTimes05").addEventListener("input", () => updateanswer("response05", "1/6", 1, "necessaryTimes05"));
+    if (comparison(fraction, integers, necessaryTimes)) {
+        document.getElementById(id).textContent = "Correto";
+    } else {
+        document.getElementById(id).textContent = "Errado";
+    }
+}
+
+function firstCheck() {
+    updateanswer("response01", "1/2", 1, "necessaryTimes01");
+    updateanswer("response02", "1/3", 1, "necessaryTimes02");
+    updateanswer("response03", "1/4", 1, "necessaryTimes03");
+    updateanswer("response04", "1/5", 1, "necessaryTimes04");
+    updateanswer("response05", "1/6", 1, "necessaryTimes05");
+}
